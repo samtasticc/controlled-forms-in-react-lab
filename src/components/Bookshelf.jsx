54ba525eb69c139b547475a6bbee3f35090d22f9
samtasticc <samantha.henesy@gmail.com>
2024-11-19 21:52:35 -0500

    define books and newBook state variables

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -1,11 +1,34 @@
 import { useState } from 'react';
 
-<div className="bookshelfDiv">
-  <div className="formDiv">
-    <h3>Add a Book</h3>
-    {/* Form will go here */}
-  </div>
-  <div className="bookCardsDiv">{/* Book cards will display here */}</div>
-</div>
+const Bookshelf = () => {
+    const [books, setBooks] = useState([
+        {
+            title: 'Shutter Island',
+            author: 'Dennis Lehane'
+        },
+        {
+            title: 'The Violence: A Novel',
+            author: 'Delilah S. Dawson',
+        }
+    ])
+    const [newBook, setNewBooks] = useState([
+        {
+            title: '',
+            author: '',
+        }
+    ])
+    return (
+        <div className="bookshelfDiv">
+            <div className="formDiv">
+                <h3>Add a Book</h3>
+                {/* Form will go here */}
+            </div>
+            <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+        </div>
+    )
+}
+
+
+
 
 export default Bookshelf;
